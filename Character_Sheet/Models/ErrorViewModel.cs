269f185d845eb5e3c74c3fdefb223fb2b0826730
Patrick Dumford <patrickdumford@hotmail.com>
2021-03-08 19:07:31 -0500

    Adding .Net Files

---
+++ Character_Sheet/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Character_Sheet.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
